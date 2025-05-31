d0b07c3adf0d37052eb9579a1389f0a094176d2f
glen-s-abraham <glenprojects666@gmail.com>
2025-05-31 10:12:04 +0530

    initial commit

---
+++ src/test/java/com/mariasorganics/farmtracker/FarmtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariasorganics.farmtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
